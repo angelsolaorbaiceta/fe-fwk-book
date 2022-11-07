198cbab864d95555105bc57e4f5d020959895dea
angel <angelsolaorbaiceta@gmail.com>
2022-11-07 08:41:28 +0100

    Rollup configuration uses single quotes

--- packages/runtime/rollup.config.mjs
+++ packages/runtime/rollup.config.mjs
@@ -3,15 +3,15 @@ import cleanup from 'rollup-plugin-cleanup'
 import filesize from 'rollup-plugin-filesize'
 
 export default {
-  input: `src/index.js`,
+  input: 'src/index.js',
   plugins: [cleanup()],
   output: [
     {
-      file: `dist/fe-fwk.js`,
+      file: 'dist/fe-fwk.js',
       format: 'esm',
     },
     {
-      file: `dist/fe-fwk.min.js`,
+      file: 'dist/fe-fwk.min.js',
       format: 'esm',
       plugins: [terser(), filesize({ showMinifiedSize: false })],
     },
