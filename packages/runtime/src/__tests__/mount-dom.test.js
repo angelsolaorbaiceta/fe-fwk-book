60a59facc5ef7605cdc7826da0be369c3bede097
angel <angelsolaorbaiceta@gmail.com>
2022-11-21 08:42:38 +0100

    test (runtime): Test dom mounting

--- packages/runtime/src/__tests__/mount-dom.test.js
+++ packages/runtime/src/__tests__/mount-dom.test.js
@@ -1,4 +1,4 @@
-import { afterEach, expect, test } from 'vitest'
+import { afterEach, expect, test, vi } from 'vitest'
 import { h, hFragment, hString } from '../h'
 import { mountDOM } from '../mount-dom'
 
@@ -18,6 +18,15 @@ test('mount a text element in a host element', () => {
   expect(document.body.innerHTML).toBe('hello')
 })
 
+test('save the created text element in the vdom', () => {
+  const vdom = hString('hello')
+  const el = mountDOM(vdom, document.body)
+
+  expect(el).toBeInstanceOf(Text)
+  expect(el.textContent).toBe('hello')
+  expect(vdom.el).toBe(el)
+})
+
 test('mount an element in a host element', () => {
   const vdom = h('div', {}, [hString('hello')])
   mountDOM(vdom, document.body)
@@ -25,9 +34,82 @@ test('mount an element in a host element', () => {
   expect(document.body.innerHTML).toBe('<div>hello</div>')
 })
 
+test('save the created element in the vdom', () => {
+  const vdom = h('div')
+  const el = mountDOM(vdom, document.body)
+
+  expect(el).toBeInstanceOf(HTMLDivElement)
+  expect(vdom.el).toBe(el)
+})
+
+test("can't mount a fragment without a parent element", () => {
+  const vdom = hFragment([hString('hello')])
+  expect(() => mountDOM(vdom)).toThrow()
+})
+
 test('mount a fragment in a host element', () => {
   const vdom = hFragment([hString('hello, '), hString('world')])
   mountDOM(vdom, document.body)
 
   expect(document.body.innerHTML).toBe('hello, world')
+})
+
+test('mount a fragment inside a host fragment', () => {
+  const host = document.createDocumentFragment()
+  const vdom = hFragment([hString('hello, '), hString('world')])
+  mountDOM(vdom, host)
+
+  expect(host.textContent).toBe('hello, world')
+})
+
+test('mount fragment with children and attributes', () => {
+  const vdom = hFragment([
+    h('span', { id: 'foo' }, [hString('hello, ')]),
+    h('span', { id: 'bar' }, [hString('world')]),
+  ])
+  mountDOM(vdom, document.body)
+
+  expect(document.body.innerHTML).toBe(
+    '<span id="foo">hello, </span><span id="bar">world</span>'
+  )
+})
+
+test('mount an element with id', () => {
+  const vdom = h('div', { id: 'foo' })
+  mountDOM(vdom, document.body)
+
+  expect(document.body.innerHTML).toBe('<div id="foo"></div>')
+})
+
+test('mount an element with class', () => {
+  const vdom = h('div', { class: 'foo' })
+  mountDOM(vdom, document.body)
+
+  expect(document.body.innerHTML).toBe('<div class="foo"></div>')
+})
+
+test('mount an element with a list of classes', () => {
+  const vdom = h('div', { class: ['foo', 'bar'] })
+  mountDOM(vdom, document.body)
+
+  expect(document.body.innerHTML).toBe('<div class="foo bar"></div>')
+})
+
+test('mount an element with event handlers', () => {
+  const onClick = vi.fn()
+  const vdom = h('div', { on: { click: onClick } })
+  mountDOM(vdom, document.body)
+
+  vdom.el?.click()
+
+  expect(onClick).toBeCalledTimes(1)
+  expect(onClick).toBeCalledWith(expect.any(MouseEvent))
+})
+
+test('mounts an element with styles', () => {
+  const vdom = h('div', { style: { color: 'red' } })
+  const el = mountDOM(vdom, document.body)
+
+  expect(document.body.innerHTML).toBe('<div style="color: red;"></div>')
+  expect(el.style.color).toBe('red')
 })
