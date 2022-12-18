98773363d4265e57ae5e053b68e449a5dfb4395c
angel <angelsolaorbaiceta@gmail.com>
2022-12-18 17:05:58 +0100

    listing: ch3

--- listings/ch03/listing_14.js
+++ listings/ch03/listing_14.js
@@ -1,18 +1,18 @@
 export function setAttributes(el, attrs) {
-  const { class: className, style, ...otherAttrs } = attrs
+  const { class: className, style, ...otherAttrs } = attrs //--1--
 
   if (className) {
-    setClass(el, className)
+    setClass(el, className) //--2--
   }
 
   if (style) {
     Object.entries(style).forEach(([prop, value]) => {
-      setStyle(el, prop, value)
+      setStyle(el, prop, value) //--3--
     })
   }
 
   for (const [name, value] of Object.entries(otherAttrs)) {
-    setAttribute(el, name, value)
+    setAttribute(el, name, value) //--4--
   }
 }
 
