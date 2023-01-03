8489f15ce3df156d8b5acd60eaf6db931f956862
angel <angelsolaorbaiceta@gmail.com>
2023-01-03 09:04:20 +0100

    Copy template files

--- packages/cli/src/init.mjs
+++ packages/cli/src/init.mjs
@@ -1,10 +1,37 @@
+import { copyFile, mkdir } from 'fs/promises'
 import { join } from 'path'
-import { mkdir } from 'fs/promises'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+const templatesDir = join(__dirname, '..', 'templates', 'init')
+const topLevelTemplates = ['package.json']
+const runtimeTemplates = [
+  '.eslintrc.js',
+  'package.json',
+  'README.md',
+  'rollup.config.mjs',
+  'vitest.config.js',
+]
 
 export default async function init(name) {
   const workingDir = process.cwd()
   const projectDir = join(workingDir, name)
+
   await mkdir(projectDir)
+  topLevelTemplates.forEach(async (template) => {
+    const srcPath = join(templatesDir, template)
+    const destPath = join(projectDir, template)
+    await copyFile(srcPath, destPath)
+  })
+
+  await initRuntime(projectDir)
 
   console.log('init', name, projectDir)
+}
+
+async function initRuntime(projectDir) {
+  const runtimeDir = join(projectDir, 'runtime')
+  await mkdir(runtimeDir)
+
+  console.log('initRuntime', projectDir)
 }
