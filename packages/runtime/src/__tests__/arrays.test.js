f07a95f462ddbc58c3bdd77722ac5a5c06c1e35d
angel <angelsolaorbaiceta@gmail.com>
2023-01-14 10:56:39 +0100

    Test arrays diff

--- packages/runtime/src/__tests__/arrays.test.js
+++ packages/runtime/src/__tests__/arrays.test.js
@@ -2,6 +2,7 @@ import { describe, expect, test } from 'vitest'
 import {
   applyArraysDiffSequence,
   arraysDiffSequence,
+  arraysDiff,
   ARRAY_DIFF_OP,
   toArray,
   withoutNulls,
@@ -18,6 +19,48 @@ test('convert non-array to array', () => {
 
 test('filter out nulls', () => {
   expect(withoutNulls([1, 2, null, 3])).toEqual([1, 2, 3])
+})
+
+describe('arrays diff', () => {
+  test('equal arrays', () => {
+    const oldArray = [1, 2, 3]
+    const newArray = [1, 2, 3]
+
+    expect(arraysDiff(oldArray, newArray)).toEqual({
+      added: [],
+      removed: [],
+    })
+  })
+
+  test('item added', () => {
+    const oldArray = [1, 2, 3]
+    const newArray = [1, 2, 3, 4]
+
+    expect(arraysDiff(oldArray, newArray)).toEqual({
+      added: [4],
+      removed: [],
+    })
+  })
+
+  test('item removed', () => {
+    const oldArray = [1, 2, 3]
+    const newArray = [1, 2]
+
+    expect(arraysDiff(oldArray, newArray)).toEqual({
+      added: [],
+      removed: [3],
+    })
+  })
+
+  test('items added and items removed', () => {
+    const oldArray = [1, 2, 3]
+    const newArray = [1, 2, 4, 5]
+
+    expect(arraysDiff(oldArray, newArray)).toEqual({
+      added: [4, 5],
+      removed: [3],
+    })
+  })
 })
 
 describe('diff sequence', () => {
