5b38f54fd540804fc45545e5a940b99385c3cf55
angel <angelsolaorbaiceta@gmail.com>
2023-03-09 09:10:59 +0100

    Split new chapter 5 into 5 and 6

--- listings/ch05/listing_03.js
+++ listings/ch05/listing_03.js
@@ -6,7 +6,7 @@ export class Dispatcher {
     if (this.#subs.has(commandName)) { // --1--
       this.#subs.get(commandName).forEach((handler) => handler(payload))
     } else {
-      console.warn(`No handlers for command: ${commandName}`)
+      console.warn(\`No handlers for command: ${commandName}`)
     }
 
     this.#afterHandlers.forEach((handler) => handler()) // --2--
