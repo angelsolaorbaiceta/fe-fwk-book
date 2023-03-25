09f97919f174c43f2c07fe0b0f2dae9f759a0aad
angel <angelsolaorbaiceta@gmail.com>
2023-03-25 20:20:33 +0100

    Add utils

--- packages/runtime/src/utils/objects.js
+++ packages/runtime/src/utils/objects.js
@@ -18,3 +18,9 @@ export function objectsDiff(oldObj, newObj) {
     ),
   }
 }
+
+// Safely evaluate `hasOwnProperty` calls.
+// https://eslint.org/docs/latest/rules/no-prototype-builtins
+export function hasOwnProperty(obj, prop) {
+  return Object.prototype.hasOwnProperty.call(obj, prop)
+}
