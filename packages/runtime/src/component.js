9463d8283b0fe99d4a85efa94c4cabfbd8d223ac
angel <angelsolaorbaiceta@gmail.com>
2023-03-26 17:10:22 +0200

    Shorten file removing comments

--- packages/runtime/src/component.js
+++ packages/runtime/src/component.js
@@ -37,38 +37,19 @@ export function defineComponent({ render, state, ...methods }) {
       this.state = state ? state(props) : {}
     }
 
-    /**
-     * Updates all or part of the component's props.
-     *
-     * @param {object} props
-     */
     updateProps(props) {
       this.props = { ...this.props, ...props }
     }
 
-    /**
-     * Updates all or part of the component's state and patches the DOM.
-     *
-     * @param {object} state
-     */
     updateState(state) {
       this.state = { ...this.state, ...state }
       this.patch()
     }
 
-    /**
-     * Renders the component, returning the virtual DOM tree representing
-     * the component in its current state.
-     */
     render() {
       return render.call(this)
     }
 
-    /**
-     * Mounts the component into the DOM.
-     *
-     * @param {HTMLElement} hostEl the host element to mount the component to
-     */
     mount(hostEl, index = null) {
       if (this.#isMounted) {
         throw new Error('Component is already mounted')
@@ -81,9 +62,6 @@ export function defineComponent({ render, state, ...methods }) {
       this.#hostEl = hostEl
     }
 
-    /**
-     * Unmounts the component from the DOM.
-     */
     unmount() {
       if (!this.#isMounted) {
         throw new Error('Component is not mounted')
@@ -95,10 +73,6 @@ export function defineComponent({ render, state, ...methods }) {
       this.#isMounted = false
     }
 
-    /**
-     * Updates the component's virtual DOM tree and patches the DOM to
-     * reflect the changes.
-     */
     patch() {
       if (!this.#isMounted) {
         throw new Error('Component is not mounted')
