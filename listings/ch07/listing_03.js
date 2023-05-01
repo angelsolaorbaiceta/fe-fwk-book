5123b9a6975cd5753083fb6851fa2f11074ba1ea
angel <angelsolaorbaiceta@gmail.com>
2023-05-01 09:32:07 +0200

    refactor (runtime): Use `includes` for simplicity

--- listings/ch07/listing_03.js
+++ listings/ch07/listing_03.js
@@ -1,10 +1,10 @@
 export function arraysDiff(oldArray, newArray) {
   return {
     added: newArray.filter(
-      (newItem) => !oldArray.some((oldItem) => newItem === oldItem) // --1--
+      (newItem) => !oldArray.includes(newItem) // --1--
     ),
     removed: oldArray.filter(
-      (oldItem) => !newArray.some((newItem) => newItem === oldItem) // --2--
+      (oldItem) => !newArray.includes(oldItem) // --2--
     ),
   }
 }
--- packages/runtime/src/utils/arrays.js
+++ packages/runtime/src/utils/arrays.js
@@ -29,12 +29,8 @@ export function withoutNulls(arr) {
  */
 export function arraysDiff(oldArray, newArray) {
   return {
-    added: newArray.filter(
-      (newItem) => !oldArray.some((oldItem) => newItem === oldItem)
-    ),
-    removed: oldArray.filter(
-      (oldItem) => !newArray.some((newItem) => newItem === oldItem)
-    ),
+    added: newArray.filter((newItem) => !oldArray.includes(newItem)),
+    removed: oldArray.filter((oldItem) => !newArray.includes(oldItem)),
   }
 }
 
