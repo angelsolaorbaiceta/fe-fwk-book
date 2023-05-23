df0defce0118620b961f2895494ffffd77ee00a2
angel <angelsolaorbaiceta@gmail.com>
2023-05-23 20:43:30 +0200

    Fix mount dom

--- packages/runtime/src/mount-dom.js
+++ packages/runtime/src/mount-dom.js
@@ -31,7 +31,7 @@ export function mountDOM(vdom, parentEl, index, hostComponent = null) {
     }
 
     case DOM_TYPES.FRAGMENT: {
-      createFragmentNodes(vdom, parentEl, hostComponent, index)
+      createFragmentNodes(vdom, parentEl, index, hostComponent)
       break
     }
 
@@ -123,6 +123,33 @@ function createFragmentNodes(vdom, parentEl, index, hostComponent) {
   children.forEach((child, i) =>
     mountDOM(child, parentEl, index ? index + i : null, hostComponent)
   )
+}
+
+/**
+ * Creates the component node, and all of its subcomponents recursively.
+ *
+ * The created `Component` is added to the `component` property of the vdom.
+ * The created `Element` is added to the `el` property of the vdom. If the component
+ * has a fragment, and thus several top-level elements, the first one is added to the `el`.
+ *
+ * The use case for the `el` reference is the reconciliation algorithm. In the case of
+ * a component, it's sole use is to move a component to a different position using the
+ * `insertBefore()` method. To insert an element before a component, the `el` property
+ * points at the component's first element.
+ *
+ * @param {import('./h').FragmentVNode} vdom the virtual DOM node of type "fragment"
+ * @param {Element} parentEl the host element to mount the virtual DOM node to
+ * @param {number} [index] the index at the parent element to mount the virtual DOM node to
+ * @param {import('./component').Component} [hostComponent] The component that the listeners are added to
+ */
+function createComponentNode(vdom, parentEl, index, hostComponent) {
+  const Component = vdom.tag
+  const { props, events } = extractComponentProps(vdom)
+  const component = new Component(props, events, hostComponent)
+
+  component.mount(parentEl, index)
+  vdom.component = component
+  vdom.el = component.firstElement
 }
 
 /**
