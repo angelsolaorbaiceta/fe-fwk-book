b1f8158a05be89a67f96486a0dd4f9d3560c938b
angel <angelsolaorbaiceta@gmail.com>
2023-05-25 16:36:04 +0200

    Fix: if child index in parent is -1, use null

--- packages/runtime/src/patch-dom.js
+++ packages/runtime/src/patch-dom.js
@@ -37,7 +37,7 @@ import { isNotBlankOrEmptyString } from './utils/strings'
  */
 export function patchDOM(oldVdom, newVdom, parentEl, hostComponent = null) {
   if (!areNodesEqual(oldVdom, newVdom)) {
-    const index = Array.from(parentEl.childNodes).indexOf(oldVdom.el)
+    const index = findIndexInParent(parentEl, oldVdom.el)
     destroyDOM(oldVdom)
     mountDOM(newVdom, parentEl, index, hostComponent)
 
@@ -66,6 +66,23 @@ export function patchDOM(oldVdom, newVdom, parentEl, hostComponent = null) {
   patchChildren(oldVdom, newVdom, hostComponent)
 
   return newVdom
+}
+
+/**
+ * Returns the index of the `el` element in its parent's children.
+ * If the element is not found, returns `null`.
+ *
+ * @param {Element} parentEl the parent element
+ * @param {Element} el the element to find
+ * @returns {number | null} index
+ */
+function findIndexInParent(parentEl, el) {
+  const index = Array.from(parentEl.childNodes).indexOf(el)
+  if (index < 0) {
+    return null
+  }
+
+  return index
 }
 
 /**
