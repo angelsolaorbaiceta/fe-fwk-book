f3af647e7ec7b425144d6042fd5a463167e4b542
angel <angelsolaorbaiceta@gmail.com>
2023-06-15 08:50:00 +0200

    Configure rollup for external dep

--- packages/runtime/rollup.config.mjs
+++ packages/runtime/rollup.config.mjs
@@ -1,9 +1,11 @@
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 import cleanup from 'rollup-plugin-cleanup'
 import filesize from 'rollup-plugin-filesize'
 
 export default {
   input: 'src/index.js',
-  plugins: [cleanup()],
+  plugins: [commonjs(), nodeResolve(), cleanup()],
   output: [
     {
       file: 'dist/fe-fwk.js',
