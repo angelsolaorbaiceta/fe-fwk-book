a10c81e04b5baacc41620379c4fb8651da876268
angel <angelsolaorbaiceta@gmail.com>
2023-06-15 07:51:15 +0200

    Config vitest

--- packages/runtime/vitest.config.js
+++ packages/runtime/vitest.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['**/__tests__/**/*.test.js'],
     reporters: 'verbose',
     environment: 'jsdom',
     coverage: {
       provider: 'v8',
-    }
+    },
   },
 })
