4942324369aa330206eea193a051214ad9f566b7
angel <angelsolaorbaiceta@gmail.com>
2023-06-27 07:55:06 +0200

    Use extract props, events for element node

--- packages/runtime/src/mount-dom.js
+++ packages/runtime/src/mount-dom.js
@@ -82,10 +82,10 @@ function createTextNode(vdom, parentEl, index) {
  * @param {import('./component').Component} [hostComponent] The component that the listeners are added to
  */
 function createElementNode(vdom, parentEl, index, hostComponent) {
-  const { tag, props, children } = vdom
+  const { tag, children } = vdom
 
   const element = document.createElement(tag)
-  addProps(element, props, vdom, hostComponent)
+  addProps(element, vdom, hostComponent)
   vdom.el = element
 
   children.forEach((child) => mountDOM(child, element, null, hostComponent))
@@ -96,12 +96,11 @@ function createElementNode(vdom, parentEl, index, hostComponent) {
  * Adds the attributes and event listeners to an element.
  *
  * @param {Element} el The element to add the attributes to
- * @param {import('./h').ElementVNodeProps} props The props to add
  * @param {import('./h').ElementVNode} vdom The vdom node
  * @param {import('./component').Component} [hostComponent] The component that the listeners are added to
  */
-function addProps(el, props, vdom, hostComponent) {
-  const { on: events, ...attrs } = props
+function addProps(el, vdom, hostComponent) {
+  const { props: attrs, events } = extractPropsAndEvents(vdom)
 
   vdom.listeners = addEventListeners(events, el, hostComponent)
   setAttributes(el, attrs)
--- packages/runtime/src/utils/promises.js
+++ packages/runtime/src/utils/promises.js
@@ -1,7 +1,3 @@
-export function toPromise(value) {
-  return Promise.resolve(value)
-}
-
 export function flushPromises() {
   return new Promise((resolve) => setTimeout(resolve))
 }
