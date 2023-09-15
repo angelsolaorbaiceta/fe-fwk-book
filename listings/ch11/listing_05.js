9a91acf405494932637ff631b3508fe4856f04e0
angel <angelsolaorbaiceta@gmail.com>
2023-09-15 19:29:35 +0200

    Update listings ch10 and ch11

--- listings/ch11/listing_05.js
+++ listings/ch11/listing_05.js
@@ -25,7 +25,7 @@ export function destroyDOM(vdom) {
     // --add--
 
     default: {
-      throw new Error(`Can't destroy DOM of type: ${type}`)
+      throw new Error(\`Can't destroy DOM of type: ${type}`)
     }
   }
 
