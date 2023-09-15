62ef9921f5058e325eaae1ed8b18d9fe8f1ad34b
Angel <angel.sola@glovoapp.com>
2023-09-15 16:24:51 +0200

    Configure compiler project

---
+++ packages/compiler/src/compile.js
@@ -0,0 +1,7 @@
+/**
+ * Compiles the given HTML template into a render function.
+ * It transforms the directives into JavaScript code.
+ *
+ * @param {string} template the HTML template to compile
+ */
+export function compileTemplate(template) {}
