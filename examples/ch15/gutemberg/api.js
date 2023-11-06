147c41461f9e13f22a52408a78f6283e7ebc0dc0
angel <angelsolaorbaiceta@gmail.com>
2023-11-06 09:07:52 +0100

    Filters

--- examples/ch15/gutemberg/api.js
+++ examples/ch15/gutemberg/api.js
@@ -2,7 +2,13 @@ const baseUrl = 'https://gutendex.com/'
 
 // https://gutendex.com/
 
-export async function getAllBooks(page = 1) {
-  const res = await fetch(`${baseUrl}/books?page=${page}`)
+export async function getAllBooks(page = 1, filters = {}) {
+  const searchParams = new URLSearchParams({ page })
+
+  if (filters.languages && filters.languages.length > 0) {
+    searchParams.set('languages', filters.languages)
+  }
+
+  const res = await fetch(`${baseUrl}/books?${searchParams}`)
   return res.json()
 }
--- examples/ch15/gutemberg/app.js
+++ examples/ch15/gutemberg/app.js
@@ -6,38 +6,65 @@ import {
 } from 'https://unpkg.com/fe-fwk@5'
 import { getAllBooks } from './api.js'
 import { BooksTable } from './components/books-table.js'
+import { Filters } from './components/filters.js'
 
 const App = defineComponent({
   state() {
     return {
       isLoading: true,
+      filters: {},
       books: [],
       page: 1,
       totalPages: 0,
+      totalBooks: 0,
     }
   },
 
   async onMounted() {
-    this.loadBooks(1)
+    await this.loadBooks(1)
   },
 
-  async loadBooks(page) {
-    this.updateState({ isLoading: true, page })
-    const books = await getAllBooks(page)
-    const totalPages = Math.ceil(books.count / 32)
+  async loadBooks(page, filters) {
+    this.updateState({
+      isLoading: true,
+      filters,
+      page,
+      totalPages: 0,
+      totalBooks: 0,
+    })
+    const books = await getAllBooks(page, filters)
+    const totalBooks = books.count
+    const totalPages = Math.ceil(totalBooks / 32)
 
     this.updateState({
       isLoading: false,
       books: books.results,
       totalPages,
+      totalBooks,
     })
   },
 
   render() {
-    const { isLoading, books, page, totalPages } = this.state
+    const { isLoading, books, page, totalPages, totalBooks } = this.state
 
     return hFragment([
       h('h1', {}, ['Gutemberg Books']),
+      h('p', {}, [
+        'This is a simple app that uses the ',
+        h('a', { href: 'https://gutendex.com/' }, ['Gutemberg API']),
+        ' to display books.',
+      ]),
+
+      h(Filters, {
+        isLoading,
+        on: {
+          langsChanged: async (languages) => {
+            await this.loadBooks(1, { languages })
+          },
+        },
+      }),
+      h('p', {}, [h('em', {}, [`Found ${totalBooks} books.`])]),
+
       h(BooksTable, {
         isLoading,
         books: books,
