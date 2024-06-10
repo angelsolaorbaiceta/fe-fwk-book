d05c084d16575c63b6441eea8281aad81141ea00
Angel <angelsolaorbaiceta@gmail.com>
2024-06-10 08:01:50 +0200

    fix (runtime): `arraysDiff()` with duplicates (#263)

--- packages/runtime/src/__tests__/arrays.test.js
+++ packages/runtime/src/__tests__/arrays.test.js
@@ -54,4 +54,58 @@ describe('arrays diff', () => {
       removed: [3],
     })
   })
+
+  test('duplicated item where one unit is removed', () => {
+    const oldArray = [1, 1]
+    const newArray = [1]
+
+    expect(arraysDiff(oldArray, newArray)).toEqual({
+      added: [],
+      removed: [1],
+    })
+  })
+
+  test('duplicated item where two items are removed', () => {
+    const oldArray = [1, 1]
+    const newArray = []
+
+    expect(arraysDiff(oldArray, newArray)).toEqual({
+      added: [],
+      removed: [1, 1],
+    })
+  })
+
+  test('duplicated item where one unit is added', () => {
+    const oldArray = [1]
+    const newArray = [1, 1]
+
+    expect(arraysDiff(oldArray, newArray)).toEqual({
+      added: [1],
+      removed: [],
+    })
+  })
+
+  test('duplicated item where two units are added', () => {
+    const oldArray = []
+    const newArray = [1, 1]
+
+    expect(arraysDiff(oldArray, newArray)).toEqual({
+      added: [1, 1],
+      removed: [],
+    })
+  })
+
+  test('array with duplicates', () => {
+    const oldArray = [1, 1, 2, 2, 3, 3, 3]
+    const newArray = [1, 1, 1, 2, 3, 4, 4]
+
+    const { added, removed } = arraysDiff(oldArray, newArray)
+    added.sort()
+    removed.sort()
+
+    expect({ added, removed }).toEqual({
+      added: [1, 4, 4],
+      removed: [2, 3, 3],
+    })
+  })
 })
